0483a89406a824071c7ec08517d1966831c5f54c
deepakkadarivel <deepak.kadarivel@gmail.com>
2018-08-12 21:39:24 +0530

    [ZMS-008] finished methedology section

--- src/components/chart/ChartComponent.js
+++ src/components/chart/ChartComponent.js
@@ -9,6 +9,69 @@ const ChartComponent = () => {
         <SectionTitleComponent
           subHeader={'all your literacy needs were in one place?'}
         />
+        <table className="chart__table">
+          <thead>
+            <tr>
+              <th />
+              <th>Zinc</th>
+              <th>Typical vocabulary program</th>
+              <th>Typical reading program</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Vocabulary instruction</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+            </tr>
+            <tr>
+              <td>Spaced repetition of new words</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+            </tr>
+            <tr>
+              <td>Authentic, unaltered text from the “real world”</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+            </tr>
+            <tr>
+              <td>Text at a variety of levels</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+              <td className="chart__table__correct">&#10004;</td>
+            </tr>
+            <tr>
+              <td>Gaming elements</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+            </tr>
+            <tr>
+              <td>Test prep</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+            </tr>
+            <tr>
+              <td>Close reading instruction</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+              <td className="chart__table__wrong">&#10007;</td>
+            </tr>
+            <tr>
+              <td>Detailed reporting</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__correct">&#10004;</td>
+              <td className="chart__table__correct">&#10004;</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div className="fullCircle" />
+        <div className="fullCircle1" />
       </section>
     </div>
   );
