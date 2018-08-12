ef9acf2453131dc80e97f55c2bdc4451ff8fd968
deepakkadarivel <deepak.kadarivel@gmail.com>
2018-08-12 23:36:09 +0530

    [ZMS-010] finished reading section

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,11 +8,101 @@ import FooterComponent from '../footer/FooterComponent';
 
 class App extends Component {
   render() {
+    const methods = {
+      contentSubHeader: 'all your literacy needs were in one place?',
+      headers: [
+        '',
+        'Zinc',
+        'Typical vocabulary program',
+        'Typical reading program'
+      ],
+      rows: [
+        {
+          title: 'Vocabulary instruction',
+          zincProg: true,
+          vocabProgram: true,
+          readingProg: false
+        },
+        {
+          title: 'Spaced repetition of new words',
+          zincProg: true,
+          vocabProgram: true,
+          readingProg: false
+        },
+        {
+          title: 'Authentic, unaltered text from the "real world"',
+          zincProg: true,
+          vocabProgram: false,
+          readingProg: false
+        },
+        {
+          title: 'Text at a variety of levels',
+          zincProg: true,
+          vocabProgram: false,
+          readingProg: true
+        },
+        {
+          title: 'Gaming elements',
+          zincProg: true,
+          vocabProgram: true,
+          readingProg: false
+        },
+        {
+          title: 'Test prep',
+          zincProg: true,
+          vocabProgram: false,
+          readingProg: false
+        },
+        {
+          title: 'Close reading instruction',
+          zincProg: true,
+          vocabProgram: false,
+          readingProg: false
+        },
+        {
+          title: 'Detailed reporting',
+          zincProg: true,
+          vocabProgram: true,
+          readingProg: true
+        }
+      ],
+      type: 'methods'
+    };
+
+    const closeReading = {
+      contentSubHeader: 'students had personalized close reading instruction?',
+      headers: ['What Zinc Close Reading offers', 'How'],
+      rows: [
+        {
+          title: 'Personalized learning',
+          content:
+            'Choose from lessons on fiction, non-fiction and poetry with a variety of reading levels, from 4th grade to post-secondary.'
+        },
+        {
+          title: 'Explicit instruction',
+          content:
+            'Learn and then strengthen the skills that make strong readers enjoy reading, turning the words into meanings and appreciating the writer’s talents.'
+        },
+        {
+          title: 'Engagement',
+          content:
+            'High school students and young adults present video lessons, and students interact with input from peers.'
+        },
+        {
+          title: 'Scaffolding',
+          content:
+            'Students work at their own pace. When they make mistakes, hints and support guide them to the right answer and full comprehension.'
+        }
+      ],
+      type: 'readings'
+    };
+
     return (
       <div>
         <HeaderComponent />
         <StepperComponent />
-        <ChartComponent />
+        <ChartComponent data={methods} />
+        <ChartComponent data={closeReading} />
         <TestimonialsComponent />
         <FooterComponent />
       </div>
--- src/components/chart/ChartComponent.js
+++ src/components/chart/ChartComponent.js
@@ -1,73 +1,17 @@
 import React from 'react';
 import './chart.css';
 import SectionTitleComponent from '../sectionTitle/SectionTitleComponent';
+import PropTypes from 'prop-types';
 
-const ChartComponent = () => {
-  const data = {
-    headers: [
-      '',
-      'Zinc',
-      'Typical vocabulary program',
-      'Typical reading program'
-    ],
-    rows: [
-      {
-        title: 'Vocabulary instruction',
-        zincProg: true,
-        vocabProgram: true,
-        readingProg: false
-      },
-      {
-        title: 'Spaced repetition of new words',
-        zincProg: true,
-        vocabProgram: true,
-        readingProg: false
-      },
-      {
-        title: 'Authentic, unaltered text from the "real world"',
-        zincProg: true,
-        vocabProgram: false,
-        readingProg: false
-      },
-      {
-        title: 'Text at a variety of levels',
-        zincProg: true,
-        vocabProgram: false,
-        readingProg: true
-      },
-      {
-        title: 'Gaming elements',
-        zincProg: true,
-        vocabProgram: true,
-        readingProg: false
-      },
-      {
-        title: 'Test prep',
-        zincProg: true,
-        vocabProgram: false,
-        readingProg: false
-      },
-      {
-        title: 'Close reading instruction',
-        zincProg: true,
-        vocabProgram: false,
-        readingProg: false
-      },
-      {
-        title: 'Detailed reporting',
-        zincProg: true,
-        vocabProgram: true,
-        readingProg: true
-      }
-    ]
-  };
+const ChartComponent = props => {
+  const { data } = props;
+  const isMethod = data.type === 'methods';
+  const isReadings = data.type === 'readings';
 
   return (
     <div className="chart">
       <section className="chart__section chart--bg parallax">
-        <SectionTitleComponent
-          subHeader={'all your literacy needs were in one place?'}
-        />
+        <SectionTitleComponent subHeader={data.contentSubHeader} />
         <table className="chart__table">
           <thead>
             <tr>
@@ -80,45 +24,64 @@ const ChartComponent = () => {
             {data.rows.map((row, i) => {
               return (
                 <tr key={i}>
-                  <td>{row.title}</td>
-                  <td
-                    className={
-                      row.zincProg
-                        ? `chart__table__correct`
-                        : `chart__table__wrong`
-                    }
-                  >
-                    {row.zincProg ? '\u2714' : '\u2717'}
+                  <td className={isReadings ? 'readingContent' : ''}>
+                    {row.title}
                   </td>
-                  <td
-                    className={
-                      row.vocabProgram
-                        ? `chart__table__correct`
-                        : `chart__table__wrong`
-                    }
-                  >
-                    {row.vocabProgram ? '\u2714' : '\u2717'}
-                  </td>
-                  <td
-                    className={
-                      row.readingProg
-                        ? `chart__table__correct`
-                        : `chart__table__wrong`
-                    }
-                  >
-                    {row.readingProg ? '\u2714' : '\u2717'}
-                  </td>
+                  {isMethod && (
+                    <td
+                      className={
+                        row.zincProg
+                          ? `chart__table__correct`
+                          : `chart__table__wrong`
+                      }
+                    >
+                      {row.zincProg ? '\u2714' : '\u2717'}
+                    </td>
+                  )}
+                  {isMethod && (
+                    <td
+                      className={
+                        row.vocabProgram
+                          ? `chart__table__correct`
+                          : `chart__table__wrong`
+                      }
+                    >
+                      {row.vocabProgram ? '\u2714' : '\u2717'}
+                    </td>
+                  )}
+                  {isMethod && (
+                    <td
+                      className={
+                        row.readingProg
+                          ? `chart__table__correct`
+                          : `chart__table__wrong`
+                      }
+                    >
+                      {row.readingProg ? '\u2714' : '\u2717'}
+                    </td>
+                  )}
+                  {isReadings && (
+                    <td className="readingContent">{row.content}</td>
+                  )}
                 </tr>
               );
             })}
           </tbody>
         </table>
 
-        <div className="fullCircle" />
-        <div className="fullCircle1" />
+        <div
+          className={`fullCircle ${isReadings ? 'fullCircle--readings' : ''}`}
+        />
+        <div
+          className={`fullCircle1 ${isReadings ? 'fullCircle1--readings' : ''}`}
+        />
       </section>
     </div>
   );
+};
+
+ChartComponent.propTypes = {
+  data: PropTypes.object
 };
 
 export default ChartComponent;
