005b22428c02840e85758e86ecd7799a1b1e2067
Deepak Kadarivel <deepak@Deepaks-MacBook-Pro.local>
2018-09-03 01:37:07 +0530

    [ZMS-036] Stepper scroll section

--- src/components/stepper/StepperComponent.js
+++ src/components/stepper/StepperComponent.js
@@ -98,53 +98,52 @@ class StepperComponent extends Component {
     };
 
     return (
-      <div className="stepper">
-        {this.state.modalOpen && (
-          <Backdrop drawerClickHandler={() => methodToggleClickHandler({})} />
-        )}
-
-        {this.state.modalOpen && <Method method={this.state.method} />}
-
-        <section className="stepper__section stepper--bg parallax">
-          <SectionTitleComponent
-            subHeader={'you had a proven methodology that works?'}
-          />
-        </section>
-
-        <div className="stepper__section__actions">
-          {methods.map(method => {
-            let text = method.text
-              .split('\n')
-              .map((i, key) => <p key={key}>{i}</p>);
-            return (
-              <div className="action-container" key={method.id}>
-                <div className="action">
-                  <div
-                    className={`action__item ${method.icon}`}
-                    onClick={() => methodToggleClickHandler(method)}
-                  />
-                  <p className="action__text">{text}</p>
+      <div className="step">
+        <div className="stepper">
+          <section className="stepper__section stepper--bg parallax">
+            <SectionTitleComponent
+              subHeader={'you had a proven methodology that works?'}
+            />
+          </section>
+          <div className="stepper__section__actions">
+            {methods.map(method => {
+              let text = method.text
+                .split('\n')
+                .map((i, key) => <p key={key}>{i}</p>);
+              return (
+                <div className="action-container" key={method.id}>
+                  <div className="action">
+                    <div
+                      className={`action__item ${method.icon}`}
+                      onClick={() => methodToggleClickHandler(method)}
+                    />
+                    <p className="action__text">{text}</p>
+                  </div>
+                  {method.id !== 4 && (
+                    <div className="action__item--link arrow" />
+                  )}
                 </div>
-                {method.id !== 4 && (
-                  <div className="action__item--link arrow" />
-                )}
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+
+          <p className="stepper__section__text">
+            Zinc addresses the literacy needs of all middle and high school
+            students, from reluctant readers to high-achievers. First, our quick
+            diagnostics TEST to establish independent reading levels. Then Zinc
+            TEACHES students close reading strategies and new vocabulary. With
+            these new skills and words, students then TRAIN their reading
+            abilities with a wide selection of leveled, authentic texts. After
+            each reading, students take a short assessment, which generates
+            robust data to TARGET instruction and monitor progress.
+          </p>
+
+          <div className="stepper__bg" />
         </div>
 
-        <p className="stepper__section__text">
-          Zinc addresses the literacy needs of all middle and high school
-          students, from reluctant readers to high-achievers. First, our quick
-          diagnostics TEST to establish independent reading levels. Then Zinc
-          TEACHES students close reading strategies and new vocabulary. With
-          these new skills and words, students then TRAIN their reading
-          abilities with a wide selection of leveled, authentic texts. After
-          each reading, students take a short assessment, which generates robust
-          data to TARGET instruction and monitor progress.
-        </p>
-
-        <div className="stepper__bg" />
+        {methods.map(method => (
+          <Method method={method} />
+        ))}
       </div>
     );
   }
